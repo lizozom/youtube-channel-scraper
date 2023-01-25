887d415227a8c4519e16b39385a8c2b1f612151d
lizozom <liza.katz@elastic.co>
2023-01-25 11:50:24 +0200

    cleanup

--- youtube.py
+++ youtube.py
@@ -1,7 +1,4 @@
-
-
 import os
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
@@ -19,21 +16,3 @@ def init():
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
     return youtube
-
-def initOauth(): 
-    scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = os.environ.get("CLIENT_SECRET_FILE_PATH")
-
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    return googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
